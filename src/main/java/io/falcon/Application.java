aafc5c0f165e531466f74da46454a160ec156cd3
mark-verebelyi <mark.verebelyi@gmail.com>
2016-10-23 11:51:27 +0200

    Add dependencies for Spring Boot Application

---
+++ src/main/java/io/falcon/Application.java
@@ -0,0 +1,16 @@
+package io.falcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mark.verebelyi@gmail.com on 2016. 10. 23.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
